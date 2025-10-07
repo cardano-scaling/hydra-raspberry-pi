f7d1ba7fbd9109718541cbb89d6a0042688eb05b
Daniel Firth <dan.firth@homotopic.tech>
2025-10-07 14:37:54 +0000

    Initial commit of hydra-raspberry-pi

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Example Raspberry Pi 5 configuration flake";
+  inputs = {
+    hydra.url = "github:cardano-scaling/hydra";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi/main";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
+
+  outputs = { self, nixpkgs, nixos-raspberrypi }@inputs:
+  let system = "aarch64-linux";
+    {
+      nixosConfigurations = {
+        hydraberry = nixos-raspberrypi.lib.nixosSystem {
+          specialArgs = inputs;
+          modules = [
+            ({ ... }: {
+              imports = with nixos-raspberrypi.nixosModules; [
+                raspberry-pi-5.base
+                raspberry-pi-5.bluetooth
+              ];
+              inherit system;
+            })
+            ({ ... }: {
+              networking.hostName = "hydraberry";
+              users.users.yourUserName = {
+                initialPassword = "yourInitialPassword";
+                isNormalUser = true;
+                extraGroups = [
+                  "wheel"
+                ];
+                environment.systemPackages = [
+                  inputs.hydra.packages.${system}.hydra-node
+                ];
+              };
+            })
+          ];
+        };
+      };
+    };
+}
